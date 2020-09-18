e3dc1321c58ff3e45969a3f81c07821b0f7d09fa
LSK <82940045@qq.com>
2020-09-18 17:13:12 +0800

    day 1

---
+++ src/pages/demo/Life.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Child from './Child'
+
+export default class Lift extends React.Component{
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         count :0
+    //     };
+    // }
+    state = {
+        count:0
+    }
+
+    handleAdd=()=> {
+        this.setState({
+            count:this.state.count + 1
+        })
+    }
+
+    handleClick() {
+        this.setState({
+            count:this.state.count + 1
+        })
+    }
+
+    render() {
+        let style = {
+            padding:50
+        }
+        return <div style={style}>
+            <p>React生命周期</p>
+            <button onClick={this.handleAdd}>点击一下</button>
+            <button onClick={this.handleClick.bind(this)}>点击一下</button>
+            <p>{this.state.count}</p>
+            <Child name={this.state.count}></Child>
+        </div>
+    }
+}
