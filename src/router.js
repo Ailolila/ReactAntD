640591f73c647bb0157425ff2fb601aedc3f7bc6
LSK <82940045@qq.com>
2020-10-16 17:13:18 +0800

    城市管理demo 第一天基础查询 ui分块

--- src/router.js
+++ src/router.js
@@ -16,6 +16,7 @@ import FormLogin from './pages/form/login';
 import FormRegister from './pages/form/register';
 import BasicTable from './pages/table/basicTable';
 import HighTable from './pages/table/highTable';
+import City from './pages/city/index';
 
 export default class IRouter extends React.Component {
 
@@ -41,6 +42,7 @@ export default class IRouter extends React.Component {
                                     <Route path="/form/reg" component={FormRegister} />
                                     <Route path="/table/basic" component={BasicTable} />
                                     <Route path="/table/high" component={HighTable} />
+                                    <Route path="/city" component={City} />
                                     <Route component={NoMatch} />
                                 </Switch>
                             </Admin>
