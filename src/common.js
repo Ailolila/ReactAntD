3ad92e64f366074d2f9cb98d08daff959506778f
LSK <82940045@qq.com>
2020-10-20 15:12:22 +0800

    订单管理-detail百度地图应用

--- src/common.js
+++ src/common.js
@@ -11,8 +11,10 @@ export default class Common extends React.Component {
                 <Row className="simple-page">
                     <Header menuType="second" />
                 </Row>
-                <Row className="content">
-                    {this.props.children}
+                <Row className="content" style={{padding:20}}>
+                    <Col span={22} offset={1} style={{ marginTop:50, marginBottom:50 }} >
+                        {this.props.children}
+                    </Col>
                 </Row>
             </div>
         );
